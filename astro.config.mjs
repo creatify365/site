c19d0a8cffe95872ce6a7f5489b27b591a7f6338
Gerald Patrick <creatify365@gmail.com>
2025-06-05 03:34:06 +0300

    Added home page with images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // Replace with your GitHub username and repository name
 const GITHUB_USERNAME = 'creatify365';
 const REPOSITORY_NAME = 'site';
 
 export default defineConfig({
   site: `https://${GITHUB_USERNAME}.github.io`,
-  base: `/${REPOSITORY_NAME}`,
+
   // other configurations
+  base: `/${REPOSITORY_NAME}`,
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
